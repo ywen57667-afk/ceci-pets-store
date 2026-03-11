5b69e77df080646311e22c1dc54bd7692d671bca
ywen57667-afk <ywen57667@gmail.com>
2026-03-11 20:12:47 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,32 +14,3 @@ const observer = new IntersectionObserver(entries => {
 products.forEach(product => {
   observer.observe(product);
 });
-document.querySelectorAll('.slider').forEach(slider => {
-
-let images = slider.querySelectorAll('img');
-let index = 0;
-
-images[0].classList.add('active');
-
-setInterval(()=>{
-
-images[index].classList.remove('active');
-
-index = (index + 1) % images.length;
-
-images[index].classList.add('active');
-
-},2000);
-
-});
-const observer = new IntersectionObserver(entries=>{
-entries.forEach(entry=>{
-if(entry.isIntersecting){
-entry.target.classList.add('show')
-}
-})
-})
-
-document.querySelectorAll('.product').forEach(p=>{
-observer.observe(p)
-})
