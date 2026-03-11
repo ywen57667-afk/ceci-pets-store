084a0f3deb565f5799c858bc23f8a2634767521e
ywen57667-afk <ywen57667@gmail.com>
2026-03-11 19:38:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 function buy(productName) {
     alert('你点击了购买：' + productName + '\n请点击支付宝支付完成付款');
 }
+const products = document.querySelectorAll('.product');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+products.forEach(product => {
+  observer.observe(product);
+});
